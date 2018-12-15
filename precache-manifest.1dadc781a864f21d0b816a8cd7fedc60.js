c971f7abe27bba4c16ebb6f328f18264b2ffd48f
shibulijack <shibulijack@gmail.com>
2018-12-15 18:03:49 +0530

    Updates

---
+++ precache-manifest.1dadc781a864f21d0b816a8cd7fedc60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5ecf79f2f5bdaf45351",
+    "url": "/cs-editor/static/js/runtime~main.a5ecf79f.js"
+  },
+  {
+    "revision": "70e4323622ff1cd2b336",
+    "url": "/cs-editor/static/js/main.70e43236.chunk.js"
+  },
+  {
+    "revision": "441e1648534e5b5b66fe",
+    "url": "/cs-editor/static/js/1.441e1648.chunk.js"
+  },
+  {
+    "revision": "70e4323622ff1cd2b336",
+    "url": "/cs-editor/static/css/main.6a264a16.chunk.css"
+  },
+  {
+    "revision": "441e1648534e5b5b66fe",
+    "url": "/cs-editor/static/css/1.b1738ea4.chunk.css"
+  },
+  {
+    "revision": "3fda9506fe801257c7b5c834517ef40c",
+    "url": "/cs-editor/index.html"
+  }
+];
